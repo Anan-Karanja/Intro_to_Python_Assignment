e9cde71abcf1c15d56a7f5be39803d4c0eaba681
Anan-Karanja <anankaranja.n@gmail.com>
2024-11-03 21:11:25 +0300

    Complete Assignment

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Declare the variables
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+location = input('Enter your location: ')
+
+# Print out the user input 
+print('Hello👋 '+name+' you are 🤗 '+ age+' years old and live in ➡️  '+ location)
